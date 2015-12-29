801bf3b2a9d2ba687625d75899e2181d2df57cfa
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2015-12-29 13:04:03 +0900

    call from instance method of library of class

--- Sources/Classes/ViewController.swift
+++ Sources/Classes/ViewController.swift
@@ -437,13 +437,7 @@ public class ViewController:UIViewController, UIScrollViewDelegate, PhotoSliderI
     }
     
     func resourceBundle() -> NSBundle {
-        let bundlePath = NSBundle.mainBundle().pathForResource(
-            "PhotoSlider",
-            ofType: "bundle",
-            inDirectory: "Frameworks/PhotoSlider.framework"
-        )
-        let bundle = NSBundle(path: bundlePath!)
-        return bundle!
+        return NSBundle(forClass: self.dynamicType)
     }
     
     // MARK: - UITraitEnvironment
