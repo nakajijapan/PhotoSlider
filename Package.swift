4e66f0b13217d07b74e71e6bd1b2cbfea16a1baa
nakajijapan <daichi.nakajima@dena.com>
2021-07-27 00:12:30 +0900

    Delete products

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 let package = Package(
     name: "PhotoSlider",
     platforms: [.iOS(.v13)],
-    products: [
-        .library(name: "PhotoSlider", targets: ["PhotoSlider"])
-    ],
     dependencies: [
         .package(url: "git@github.com:onevcat/Kingfisher.git", from: "6.3.0")
     ]
