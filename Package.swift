2d4c7c27481acc68939bf5b85246d96d7f6fa589
nakajijapan <daichi.nakajima@dena.com>
2021-07-27 00:24:43 +0900

    add projects

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 let package = Package(
     name: "PhotoSlider",
     platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "PhotoSlider", targets: ["PhotoSlider"])
+    ],
     dependencies: [
         .package(url: "git@github.com:onevcat/Kingfisher.git", from: "6.3.0")
     ]
